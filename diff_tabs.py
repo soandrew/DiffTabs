80b6cdc5f0ac5efb29c602380d200984baab9f79
Andrew So <soandrew@users.noreply.github.com>
2016-12-21 14:54:03 -0500

    Compare buffers instead of files to address issue #1 (comparing unsaved files)

--- diff_tabs.py
+++ diff_tabs.py
@@ -1,18 +1,41 @@
 # coding: utf-8
+import sublime
 import sublime_plugin
+import difflib
+import time
+import os.path
 
 class DiffTabsCommand(sublime_plugin.WindowCommand):
     def run(self, group, index):
         window = self.window
-        view = window.active_view()
 
-        if window.get_view_index(view) == (group, index):
+        aview = window.active_view()
+        if window.get_view_index(aview) == (group, index):
             return
+        bview = window.views_in_group(group)[index]
 
-        this_file = view.file_name()
-        other_file = window.views_in_group(group)[index].file_name()
+        aname = aview.file_name() if aview.file_name() else aview.name()
+        bname = bview.file_name() if bview.file_name() else bview.name()
 
-        window.run_command("diff_files", {"files": [this_file, other_file]})
+        a = aview.substr(sublime.Region(0, aview.size())).splitlines()
+        b = bview.substr(sublime.Region(0, bview.size())).splitlines()
+
+        adate = time.ctime() if aview.is_dirty() else time.ctime(os.stat(aname).st_mtime)
+        bdate = time.ctime() if bview.is_dirty() else time.ctime(os.stat(bname).st_mtime)
+
+        diff = difflib.unified_diff(b, a, bname, aname, bdate, adate, lineterm='')
+        difftxt = u"\n".join(line for line in diff)
+
+        if difftxt == "":
+            sublime.status_message("Files are identical")
+        else:
+            v = window.new_file()
+            v.set_name(os.path.basename(bname) + " -> " + os.path.basename(aname))
+            v.set_scratch(True)
+            v.set_syntax_file('Packages/Diff/Diff.tmLanguage')
+            edit = v.begin_edit()
+            v.insert(edit, 0, difftxt)
+            v.end_edit(edit)
 
     def is_visible(self, group, index):
         window = self.window
